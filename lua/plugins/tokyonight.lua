739d14bbf522b066209515655d8bbb25f070339c
vladimirivanov <vl.ivanov1232@gmail.com>
2024-06-28 12:27:06 +0400

    added some plugins

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -1,5 +1,16 @@
 return {
   "folke/tokyonight.nvim",
   lazy = true,
-  opts = { style = "night" },
+  opts = {
+    style = "moon",
+    styles = {},
+    on_colors = function(colors)
+      colors.comment = "#d1bebe"
+    end,
+    on_highlights = function(highlights, colors)
+      highlights.LineNr = { fg = colors.yellow }
+      highlights.LineNrBelow = { fg = "#eddfdf" }
+      highlights.LineNrAbove = { fg = "#eddfdf" }
+    end,
+  },
 }
